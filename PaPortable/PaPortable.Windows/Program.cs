19ec7c04f1effb22e8484b240470fb66195f023b
gtryus <gtryus@gmail.com>
2017-12-20 18:09:26 -0600

    Add a splash screen form

--- PaPortable/PaPortable.Windows/Program.cs
+++ PaPortable/PaPortable.Windows/Program.cs
@@ -1,50 +1,107 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Drawing;
-using System.IO;
 using Gecko;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
 using SIL.IO;
 using PaPortable.Views;
-using System.Reflection;
+using System.Drawing;
+using System.Threading;
+using System.ComponentModel;
 
-namespace PaPortable
+namespace PaPortable.Windows
 {
     static class Program
     {
+        static List<String> _supportFile = new List<string>();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Xpcom.Initialize("Firefox");
-            var geckoWebBrowser = new GeckoWebBrowser { Dock = DockStyle.Fill };
-            Form f = new Form { Size = new Size(750, 725) };
-            f.Controls.Add(geckoWebBrowser);
+            CreateResources();
+            DisplaySplash();
+            DisplayData();
+            foreach (var fullPath in _supportFile)
+            {
+                File.Delete(fullPath);
+            }
+        }
 
+        private static void DisplaySplash()
+        {
+            var f = new Form { Size = new Size(550, 200) };
+            Gecko.GeckoWebBrowser browser = new Gecko.GeckoWebBrowser { Dock = DockStyle.Fill };
+            f.Controls.Add(browser);
+            var template = new Splash() { Model = 0 };
+            var page = template.GenerateString();
+            using (var tempFile = TempFile.WithExtension("html"))
+            {
+                File.WriteAllText(tempFile.Path, page);
+                var uri = new Uri(tempFile.Path);
+                browser.Navigate(uri.AbsoluteUri);
+                f.Show();
+                var worker = new BackgroundWorker();
+                worker.DoWork += _DoWork;
+                worker.RunWorkerAsync();
+                while (worker.IsBusy) Application.DoEvents();
+            }
+            f.Close();
+        }
+
+        private static void _DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(1000);
+        }
+
+        private static void DisplayData()
+        {
+            var f = new Form { Size = new Size(750, 725) };
+            Gecko.GeckoWebBrowser browser = new Gecko.GeckoWebBrowser { Dock = DockStyle.Fill };
+            f.Controls.Add(browser);
             var model = new Lip3Data().MyRecs;
             var template = new DataCorpus() { Model = model };
             var page = template.GenerateString();
-            using (var tempfile = TempFile.WithExtension("html"))
+            using (var tempFile = TempFile.WithExtension("html"))
             {
-                File.WriteAllText(tempfile.Path, page);
-                var uri = new Uri(tempfile.Path);
-                var folder = Path.GetDirectoryName(tempfile.Path);
-                WriteResource(folder, "Content", "bootstrap.css");
-                WriteResource(folder, "Scripts", "bootstrap.js");
-                WriteResource(folder, "Scripts", "jquery-1.9.1.js");
-                geckoWebBrowser.Navigate(uri.AbsoluteUri);
+                File.WriteAllText(tempFile.Path, page);
+                var uri = new Uri(tempFile.Path);
+                browser.Navigate(uri.AbsoluteUri);
                 Application.Run(f);
             }
+        }
+
+        private static void CreateResources()
+        {
+            var folder = Path.GetTempPath();
+            WriteResource(folder, "Content", "bootstrap.css");
+            WriteResource(folder, "Content", "PA-64x64.png");
+            WriteResource(folder, "Scripts", "bootstrap.js");
+            WriteResource(folder, "Scripts", "jquery-1.9.1.js");
         }
 
         private static void WriteResource(string folder, string projectLocation, string name)
         {
             using (var str = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("PaPortable." + projectLocation + "." + name)))
             {
-                File.WriteAllText(Path.Combine(folder, name), str.ReadToEnd());
+                var fullPath = Path.Combine(folder, name);
+                var buffer = new byte[1000];
+                using (var os = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
+                {
+                    int count;
+                    do
+                    {
+                        count = str.BaseStream.Read(buffer, 0, 1000);
+                        os.Write(buffer, 0, count);
+                    } while (count > 0);
+                }
+                _supportFile.Add(fullPath);
             }
         }
 
